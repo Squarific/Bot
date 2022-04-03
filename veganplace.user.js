523b16311b308f38119d1b43e1c5066e75e18fc4
Avery Smets <filipsmetsbelgium@gmail.com>
2022-04-03 21:23:51 +0200

    Update veganplace.user.js

--- veganplace.user.js
+++ veganplace.user.js
@@ -259,8 +259,8 @@ function place(x, y, color) {
                     'actionName': 'r/replace:set_pixel',
                     'PixelMessageData': {
                         'coordinate': {
-                            'x': x % 1000,
-                            'y': y % 1000
+                            'x': x % 2000,
+                            'y': y % 2000
                         },
                         'colorIndex': color,
                         'canvasIndex': (x > 999 ? 1 : 0)
