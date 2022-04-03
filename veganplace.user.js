be8ce3dea2ab47041ab5d2c918b0a4c8b6e147d6
sammie <Alphasam123@gmail.com>
2022-04-03 20:43:41 +0100

    increment version

--- veganplace.user.js
+++ veganplace.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VeganPlace Bot
 // @namespace    https://github.com/Squarific/Bot
-// @version      27
+// @version      28
 // @description  The bot for vegans
 // @author       Squarific
 // @match        https://www.reddit.com/r/place/*
