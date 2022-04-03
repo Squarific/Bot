65c4e6002c6e0f56eb13d1a0f8700f5a864d4567
Avery Smets <filipsmetsbelgium@gmail.com>
2022-04-03 21:27:40 +0200

    Update veganplace.user.js

--- veganplace.user.js
+++ veganplace.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VeganPlace Bot
 // @namespace    https://github.com/Squarific/Bot
-// @version      25
+// @version      26
 // @description  The bot for vegans
 // @author       Squarific
 // @match        https://www.reddit.com/r/place/*
